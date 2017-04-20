099190471b91dc172614f2f4841189040a043b9d
Ramona Bejan <bejan2ramona@gmail.com>
2017-04-20 20:09:42 +0100

    see users by location + stats

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
 4. Run `npm run test` to get the program running (modify the user and password if needed)
 5. Examine the typescript code under `server.ts`
 
+===
+
+
+test add user with command line argument: 
+	
+	npm run test -- -u [username]	
+
+test list users by location
+	npm run test -- -l [location]
+	npm run test -- -l Lisbon
+
+After each command we can see stats by location
+
--- server.ts
+++ server.ts
@@ -2,8 +2,25 @@ const pgPromise = require('pg-promise');
 const R         = require('ramda');
 const request   = require('request-promise');
 
+
 // Limit the amount of debugging of SQL expressions
 const trimLogsSize : number = 200;
+
+//parse command line arguments
+const argv = require('minimist')(process.argv.slice(2));
+
+console.dir(argv);
+
+const username= argv['u'];
+const loc= argv['l'];
+
+
+if(username === undefined && loc === undefined ) {
+  console.error("Please profide at least one parameter: -u 'github_username', -l 'location'");
+  process.exit(0);
+
+}
+
 
 // Database interface
 interface DBOptions
@@ -22,14 +39,6 @@ const options : DBOptions = {
   database: 'lovelystay_test',
 };
 
-
-
-const username= process.argv[2];
-
-if(username == undefined) {
-  console.error("Please profide a valid username");
-  process.exit();
-}
 
 console.info('Connecting to the database:',
   `${options.user}@${options.host}:${options.port}/${options.database}`);
@@ -55,15 +64,79 @@ interface GithubUsers
 const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
 
-db.none('CREATE TABLE IF NOT EXISTS github_users  (id BIGSERIAL, login TEXT UNIQUE, name TEXT, company TEXT, location TEXT, email TEXT)')
-.then(() => request({
-  uri: `https://api.github.com/users/${username}`,
-  headers: {
-        'User-Agent': 'Request-Promise'
-    },
-  json: true
-}))
-.then((data: GithubUsers) => db.one(
-  'INSERT INTO github_users (login,name,company,location) VALUES ($[login], $[name],  $[company], $[location]) RETURNING id', data)
-).then(({id}) => console.log(id))
-.then(() => process.exit(0));
+//insert user in db
+if(username !== undefined) {
+
+    db.none('CREATE TABLE IF NOT EXISTS github_users  (id BIGSERIAL, login TEXT UNIQUE, name TEXT, company TEXT, location TEXT, email TEXT)')
+  .then(() => request({
+    uri: `https://api.github.com/users/${username}`,
+    headers: {
+          'User-Agent': 'Request-Promise'
+      },
+    json: true
+  }))
+  .then((data: GithubUsers) => db.one(
+    'INSERT INTO github_users (login,name,company,location) VALUES ($[login], $[name],  $[company], $[location]) RETURNING id', data)
+  ).then(({id}) => console.log(id))
+   .catch(error => {
+        console.error(error);
+        process.exit(1);
+
+    })
+  .then(() => {
+    showStats();
+   });
+
+}
+//show users by location
+if(loc !== undefined) {
+  console.info(`=====GitHub users in ${loc}`)
+  db.each(`SELECT login,name FROM github_users WHERE location LIKE'%${loc}%'`, [], row => {
+    row.login += " ";
+    ;
+  })
+    .then(data => {
+        data.map(result => {
+         console.log(`${result.login}  ${result.name}` );
+
+        });
+    })
+
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+
+    })
+     .then(() => {
+        showStats();
+     });
+ }
+
+
+//show stats by location
+var showStats = function() {
+    console.info("======Stats by location: ")
+    db.each(`SELECT location,count(login) FROM github_users group by location`, [], row => {
+    row.login += " ";
+    ;
+  })
+    .then(data => {
+        data.map(result => {
+         console.log(`${result.location}  ${result.count}` );
+
+        });
+    })
+
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+
+    })
+     .then(() => process.exit(0));
+}
+
+ 
+
+   
+
+
